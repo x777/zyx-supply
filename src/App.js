823311d7241c910bbff93099b08b38ef54194f72
x777 <x777>
2021-11-02 12:41:34 -0400

    Added web3 FromWei calculation and added TOTAL value

--- src/App.js
+++ src/App.js
@@ -1,73 +1,90 @@
 /* eslint-disable no-undef */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import axios from "axios";
 import SupplyCard from "./Card";
+import Web3 from "web3";
 
+const web3 = new Web3();
 const ADDR_LIST = [
   "0x0000000000000000000000000000000000001000",
-  "0x0000000000000000000000000000000000002002",
   "0x0000000000000000000000000000000000002001",
+  "0x0000000000000000000000000000000000002002",
   "0x7191c48d168dB12121C01f5C645e0c2cd0A030ec",
 ];
 
-const TOTAL_SUPPLY = 900000000 * Math.pow(10, 10);
+const SPECIAL_ACCOUNTS = ["0x0000000000000000000000000000000000001000"];
+
+// var TOTAL_SUPPLY = 900000000 * Math.pow(10, 10);
 
 function App() {
-  const [totalBalance, setTotalBalance] = useState(null);
-  const [cirkSupply, setCirkSupply] = useState(null);
-
-  const getData = async () => {
-    try {
-      const { data } = await axios.get(
-        `https://zyxscan.com/api/?module=account&action=balancemulti&address=${ADDR_LIST}`
-      );
-
-      console.log(data);
-
-      let summaryBalance = 0;
-      data.result.map((item) => {
-        summaryBalance += parseInt(item.balance) / Math.pow(10, 8);
-        console.log(summaryBalance);
-      });
-      let cirulatingSupply =
-        (TOTAL_SUPPLY - summaryBalance) / Math.pow(10, 8) / 100;
-      console.log(TOTAL_SUPPLY);
-      console.log(cirulatingSupply);
-      console.log(summaryBalance > TOTAL_SUPPLY);
-
-      setCirkSupply(cirulatingSupply);
-      setTotalBalance(summaryBalance);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+  const FIXED_TOTAL = 900000000;
+  const [locked, setLocked] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [cirkulatedSupply, setCirkSupply] = useState(null);
 
   useEffect(() => {
+    async function getData() {
+      try {
+        const { data } = await axios.get(
+          `https://zyxscan.com/api/?module=account&action=balancemulti&address=${ADDR_LIST}`
+        );
+
+        console.log(data);
+
+        // summary balance for all accounts
+        let summaryBalance = 0;
+        // summary balance of special accounts
+        let specialAccountsSummBalance = 0;
+        data.result.map((item) => {
+          summaryBalance += parseInt(web3.utils.fromWei(item.balance)); // / Math.pow(10, 8);
+          // Check if account is SPECIAL
+          SPECIAL_ACCOUNTS.map((account) => {
+            if (account == item.account) {
+              // Summ special account balance
+              specialAccountsSummBalance += parseInt(
+                web3.utils.fromWei(item.balance)
+              );
+            }
+          });
+        });
+
+        // "TOTAL" value
+        let total = FIXED_TOTAL - specialAccountsSummBalance;
+
+        // circulating supply
+        let cirulatingSupply = FIXED_TOTAL - summaryBalance;
+
+        // locked supply
+        let locked = FIXED_TOTAL - cirkulatedSupply;
+
+        setTotal(total);
+        setCirkSupply(cirulatingSupply);
+        setLocked(locked);
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
     getData();
     const interval = setInterval(() => {
       getData();
-    }, 10000);
+    }, 15000);
 
     return () => clearInterval(interval);
-  }, [cirkSupply]);
+  }, [cirkulatedSupply]);
 
   return (
     <div>
       {/* <BasicTable/> */}
-      {cirkSupply && totalBalance && (
+      {cirkulatedSupply && (
         <div>
-          <SupplyCard supply={cirkSupply} title="Circulating supply" />
+          <SupplyCard supply={cirkulatedSupply} title="Circulating supply" />
           <br />
-          <SupplyCard
-            supply={TOTAL_SUPPLY / Math.pow(10, 10)}
-            title="Total supply"
-          />
+          <SupplyCard supply={FIXED_TOTAL} title="Max supply" />
           <br />
-          <SupplyCard
-            supply={TOTAL_SUPPLY / Math.pow(10, 10) - cirkSupply}
-            title="Locked"
-          />
+          <SupplyCard supply={locked} title="Locked" />
+          <br />
+          <SupplyCard supply={total} title="Total" />
           {/* <h4>Total balance on accounts: {totalBalance}</h4> */}
         </div>
         // <ul>
